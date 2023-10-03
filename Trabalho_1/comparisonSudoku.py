a3d70be11f8c63141069b28a15a9cd2422c3c35b
Micael <mikal2003.sp@gmail.com>
2023-10-03 00:16:06 -0300

    fazendo dfs para otimizar o codigo

--- Trabalho_1/comparisonSudoku.py
+++ Trabalho_1/comparisonSudoku.py
@@ -50,22 +50,40 @@ def is_a_valid_move(matrix, row, col, num, matrix_comparation):
 
     return True
 
-#matriz de comparacao
-matriz_comp = [[-1, 1, 1, -1], [-1, 0, 0, 0], [-1, -1, 0, 1], [-1, 1, 1, -1], [-1, 1, 1, 0], [-1, -1, 0, 0], [-1, 0, 0, -1], [-1, 1, 0, 1], [-1, -1, 1, 0], [0, 0, 0, -1], [1, 1, 1, 1], [1, -1, 0, 0], [0, 0, 0, -1], [0, 1, 1, 1], [1, -1, 0, 0], [1, 0, 0, -1], [1, 1, 1, 1], [0, -1, 1, 0], [1, 0, -1, -1], [0, 0, -1, 1], [1, -1, -1, 1], [1, 1, -1, -1], [0, 0, -1, 0], [1, -1, -1, 1], [1, 1, -1, -1], [0, 1, -1, 0], [0, -1, -1, 0], [-1, 0, 0, -1], [-1, 1, 1, 1], [-1, -1, 0, 0], [-1, 1, 1, -1], [-1, 1, 1, 0], [-1, -1, 1, 0], [-1, 0, 1, -1], [-1, 1, 1, 1], [-1, -1, 0, 0], [1, 1, 1, -1], [0, 0, 1, 0], [1, -1, 1, 1], [0, 1, 1, -1], [0, 1, 0, 0], [0, -1, 0, 0], [0, 1, 0, -1], [0, 0, 0, 0], [1, -1, 1, 1], [0, 1, -1, -1], [0, 0, -1, 0], [0, -1, -1, 1], [0, 0, -1, -1], [1, 0, -1, 1], [1, -1, -1, 1], [1, 0, -1, -1], [1, 0, -1, 1], [0, -1, -1, 1], [-1, 0, 0, -1], [-1, 0, 1, 1], [-1, -1, 1, 1], [-1, 0, 0, -1], [-1, 0, 1, 1], [-1, -1, 1, 1], [-1, 0, 0, -1], [-1, 1, 1, 1], [-1, -1, 1, 0], [1, 1, 1, -1], [0, 0, 0, 0], [0, -1, 1, 1], [1, 1, 1, -1], [0, 0, 0, 0], [0, -1, 1, 1], [1, 1, 1, -1], [0, 0, 0, 0], [0, -1, 0, 1], [0, 0, -1, -1], [1, 1, -1, 1], [0, -1, -1, 0], [0, 0, -1, -1], [1, 1, -1, 1], [0, -1, -1, 0], [0, 0, -1, -1], [1, 0, -1, 1], [1, -1, -1, 1]]
+def limits(position, matrix_comp, visited, lim):
+    #fazendo dfs para ver quantos numeros sao menores ou maiores que a celula atual
+    #dessa forma eu nao preciso iterar de 1 ate 9 toda a vez para achar a celula ideal
+    #a ideia eh se o numero foi maior que alguma celula e aquela celula for maior que outra, entao ele eh pelo menos 3, e assim segue a ideia
+    #se ele for menor que alguma celula e aquela celula for menor que outra, entao ele eh pelo menos 7, e assim segue a ideia
+    total = 1
+    visited.append(position)
+    if matrix_comp[position][0] == lim and position-9 not in visited:
+        total += limits(position-9, matrix_comp, visited, lim)
+    if matrix_comp[position][1] == lim and position+1 not in visited:
+        total += limits(position+1, matrix_comp, visited, lim)
+    if matrix_comp[position][2] == lim and position+9 not in visited:
+        total += limits(position+9, matrix_comp, visited, lim)
+    if matrix_comp[position][3] == lim and position-1 not in visited:
+        total += limits(position-1, matrix_comp, visited, lim)
 
-def solve(matrix):
+    return total
+
+
+
+
+def solve(matrix, possibilities_matrix, matrix_comp):
     for row in range(9):
         for col in range(9):
             #so vejo celulas vazias
             if matrix[row][col] == 0:
-                for num in range(1,10):
+                for num in possibilites_matrix[row*9+col]:
                     #testo todos os numeros de 1 a 9
                     # e vejo se eh valido
-                    if is_a_valid_move(matrix, row, col, num, matriz_comp):
+                    if is_a_valid_move(matrix, row, col, num, matrix_comp):
                         #caso for adiciono ele
                         matrix[row][col] = num
                         #chamo a funcao recursivamente para a proxima celula
-                        if solve(matrix):
+                        if solve(matrix, possibilities_matrix, matrix_comp):
                             # caso ele conseguir resolver
                             # todas as recursoes, retorna true
                             return True
@@ -77,10 +95,24 @@ def solve(matrix):
                 return False
     return True
 
+def get_possibilities_matrix(matrix_comp):
+    matrix = []
+    for i in range(81):
+        element = []
+        #vejo as possibilidade de cada celula seguindo a ideia explicada nos limites
+        #naquele caso exemplo que coloquei comentando, aqui iteraria por 3 ate 8(pois precisa contar o 7)
+        for j in range(limits(i, matrix_comp, [], 1), 11-limits(i, matrix_comp, [], 0)):
+            element.append(j)
+        matrix.append(element)
+    return matrix
+
 if __name__ == '__main__':
     start = time.time()
     matrix = [[0 for j in range(9)]for i in range(9)]
-    if solve(matrix):
+    #matriz de comparacao
+    matrix_comp = [[-1, 1, 1, -1], [-1, 0, 0, 0], [-1, -1, 0, 1], [-1, 1, 1, -1], [-1, 1, 1, 0], [-1, -1, 0, 0], [-1, 0, 0, -1], [-1, 1, 0, 1], [-1, -1, 1, 0], [0, 0, 0, -1], [1, 1, 1, 1], [1, -1, 0, 0], [0, 0, 0, -1], [0, 1, 1, 1], [1, -1, 0, 0], [1, 0, 0, -1], [1, 1, 1, 1], [0, -1, 1, 0], [1, 0, -1, -1], [0, 0, -1, 1], [1, -1, -1, 1], [1, 1, -1, -1], [0, 0, -1, 0], [1, -1, -1, 1], [1, 1, -1, -1], [0, 1, -1, 0], [0, -1, -1, 0], [-1, 0, 0, -1], [-1, 1, 1, 1], [-1, -1, 0, 0], [-1, 1, 1, -1], [-1, 1, 1, 0], [-1, -1, 1, 0], [-1, 0, 1, -1], [-1, 1, 1, 1], [-1, -1, 0, 0], [1, 1, 1, -1], [0, 0, 1, 0], [1, -1, 1, 1], [0, 1, 1, -1], [0, 1, 0, 0], [0, -1, 0, 0], [0, 1, 0, -1], [0, 0, 0, 0], [1, -1, 1, 1], [0, 1, -1, -1], [0, 0, -1, 0], [0, -1, -1, 1], [0, 0, -1, -1], [1, 0, -1, 1], [1, -1, -1, 1], [1, 0, -1, -1], [1, 0, -1, 1], [0, -1, -1, 1], [-1, 0, 0, -1], [-1, 0, 1, 1], [-1, -1, 1, 1], [-1, 0, 0, -1], [-1, 0, 1, 1], [-1, -1, 1, 1], [-1, 0, 0, -1], [-1, 1, 1, 1], [-1, -1, 1, 0], [1, 1, 1, -1], [0, 0, 0, 0], [0, -1, 1, 1], [1, 1, 1, -1], [0, 0, 0, 0], [0, -1, 1, 1], [1, 1, 1, -1], [0, 0, 0, 0], [0, -1, 0, 1], [0, 0, -1, -1], [1, 1, -1, 1], [0, -1, -1, 0], [0, 0, -1, -1], [1, 1, -1, 1], [0, -1, -1, 0], [0, 0, -1, -1], [1, 0, -1, 1], [1, -1, -1, 1]]
+    possibilites_matrix = get_possibilities_matrix(matrix_comp)
+    if solve(matrix, possibilites_matrix, matrix_comp):
         for i in matrix:
             print(i)
         end = time.time()
